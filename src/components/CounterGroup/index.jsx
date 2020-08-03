b0d7526c181e0952bded268d50ee126b9edbe4d5
KiKiYang97 <dunka97@163.com>
2020-08-03 16:19:21 +0800

    [Kiki]test:git push test

--- src/components/CounterGroup/index.jsx
+++ src/components/CounterGroup/index.jsx
@@ -4,13 +4,25 @@ import Counter from '../Counter';
 class CounterGroup extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            number : 0
+        }
+    }
+    onInputChange = (e) =>{
+        console.log(typeof e.target.value)
+        this.setState({
+            number : Number(e.target.value)
+        })
     }
     render() {
         return (
 
             <div>
+                <div>
+                    number of contents : <input onChange={this.onInputChange}></input>
+                </div>
                 {
-                    new Array(this.props.value).fill(0).map((value, index) => 
+                    new Array(this.state.number).fill(0).map((value, index) => 
                         <Counter key ={index} /> 
                     )
                 }
