a8e95c2ce889be2b174abd9bbcfe80b67dcaa06c
YANGKI4 <dunka97@163.com>
2020-08-03 15:10:36 +0800

    [CounterGroup][Kiki]refactor : add Counter Group

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+class Counter extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            number : 0
+        }
+        console.log("constructor")
+    }
+    componentDidMount() {
+        console.log("componentDidMount")
+    }
+  
+    componentWillUnmount() {
+        console.log("componentWillUnmount")
+    }
+    onIncreaseClick = () => {
+        this.setState((preState) =>({
+            number : preState.number + 1
+        }));
+    }
+    onReduceClick = () => {
+        this.setState((preState) => ({
+            number : preState.number - 1
+        }))
+    }
+    render(){
+        return(
+            <div className="counter">
+                <button onClick = {this.onIncreaseClick}>+</button>
+                <mark>{this.state.number}</mark>
+                <button onClick = {this.onReduceClick}>-</button>
+            </div>
+        );
+    }
+}
+export default  Counter;
